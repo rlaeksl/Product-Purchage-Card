470de3aaced3f6e8a2406c80ce90db6bc76245d7
rlaeksl <107830031+rlaeksl@users.noreply.github.com>
2023-02-26 16:54:56 +0900

    ProductPage 설정

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -4,29 +4,36 @@ import './../style/productPage.css';
 const ProductPage = () => {
   return (
     <main>
-      <img
-        className="product-img"
-        src="https://velog.velcdn.com/images/leejh4197/post/ffe9e3fa-0571-4c0f-b31d-80c0d51a6d00/image.png"
-      />
-      <h1>래피젠 코로나 자가 검사 키트 2개입X1박스</h1>
-      <div className="price-wrap">
-        <p>88%</p>
-        <p>
-          <span>56,760원</span>6,370원
-        </p>
+      <div className="product-info">
+        <img
+          className="product-img"
+          src="https://velog.velcdn.com/images/leejh4197/post/ffe9e3fa-0571-4c0f-b31d-80c0d51a6d00/image.png"
+        />
+        <h1 className="product-title">
+          <p>래피젠 코로나 자가 검사 키트</p>
+          <p>2개입X1박스</p>
+        </h1>
+        <div className="price-wrap">
+          <p className="product-discount">88%</p>
+          <p className="product-price">
+            <span className="product-original-price">56,760원</span>6,370원
+          </p>
+        </div>
       </div>
-      <div>
-        <span>택배배송</span>
-        <span>3,000원</span>
-        <span>(주문시 결제)</span>
-        <p>10개마다 부과</p>
+      <div className="product-delivery-charge">
+        <span className="product-delivery-parcel">택배배송</span>
+        <span className="product-delivery-price">3,000원</span>
+        <span className="product-delivery-payment-method">(주문시 결제)</span>
+        <p className="product-delivery-count-method">10개마다 부과</p>
       </div>
-      <div>
-        <button>-</button>
-        <div>1</div>
-        <button>+</button>
+      <div className="product-quantity">
+        <div className="product-qunatity-button">
+          <button className="product-quantity-minus">-</button>
+          <div className="product-quantity-number">1</div>
+          <button className="product-quantity-plus">+</button>
+        </div>
       </div>
-      <div>
+      <div className="product-purchase-wrap">
         <p>총 상품 금액</p>
         <p>
           <span>총 수량 1개</span>6,370원
